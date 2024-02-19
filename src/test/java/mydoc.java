231514dc680f0d07224a16e65e050b6a51d46ba9
Jafar Hussain Shaik <shaikjafarhussain071@gmail.com>
2024-02-19 18:09:19 +0530

    first commit

---
+++ src/test/java/mydoc.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class mydoc {
+    @Test(priority = 5)
+    public static void shoppingCartTest() {
+        // Implement shopping cart test scenario
+        // Navigate to book catalog page
+        // Add books to shopping cart
+        // Verify selected items and total price in the cart
+        // Remove items from the cart
+        // Verify cart is updated accordingly
+    }
+}
